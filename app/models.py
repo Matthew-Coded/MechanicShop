8dce72514b74f5985f651da0bc4249d6c6c17258
Matthew-Coded <god@Gods-MacBook-Pro.local>
2026-01-28 14:01:44 -0800

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,47 @@
-from sqlalchemy import String, Integer, Date, ForeignKey, Column, Table
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, Date, ForeignKey, Column, Table, Float, Numeric, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
+from decimal import Decimal
 
+
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(250), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(250), nullable=False)
+    email: Mapped[str] = mapped_column(String(250), nullable=False)
+    phone: Mapped[str] = mapped_column(String(20), nullable=False)
+    address: Mapped[str] = mapped_column(String(500), nullable=False)
+
+
+class ServiceTicket(Base):
+    __tablename__ = 'service_tickets'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    service_desc: Mapped[str] = mapped_column(Text, nullable=False)
+
+
+
+class TicketMechanic(Base):
+    __tablename__ = 'ticket_mechanic'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+
+class Mechanic(Base):
+    __tablename__ = 'mechanics'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(250), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(250), nullable=False)
+    email: Mapped[str] = mapped_column(String(250), nullable=False)
+    phone: Mapped[str] = mapped_column(String(20), nullable=False)
+    address: Mapped[str] = mapped_column(String(500), nullable=False)
+    salary: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
